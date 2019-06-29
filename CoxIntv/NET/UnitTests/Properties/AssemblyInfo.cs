07b77e773844462b3196b9f9104f46646ff1b1f7
TaraE <ttelsey@gmail.com>
2019-06-28 17:59:37 -0700

    Started some unit tests

---
+++ CoxIntv/NET/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6db03d27-43eb-459e-9099-f04f87e3cffa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CoxIntv/NET/UnitTests/SubmitAnswerServiceTests.cs
@@ -0,0 +1,114 @@
+﻿using CoxIntv.ApiService;
+using CoxIntv.Model.DataSet;
+using CoxIntv.Model.Vehicles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SubmitAnswerServiceTests
+    {
+        [TestMethod]
+        public void SubmitNewAnswer_CreateDataSet_Throws()
+        {
+            // Arrange
+            IOException expectedException = new IOException("failed...");
+            Mock<ICoxIntvApiService> apiService = new Mock<ICoxIntvApiService>();
+            apiService.Setup(x => x.CreateDataSet()).Returns(Task.FromException<DataSet>(expectedException));
+            ISubmitAnswerService answerService = new SubmitAnswerService(apiService.Object);
+
+
+            // Act
+            try
+            {
+                Task<AnswerServiceResponse> answerTask = answerService.SubmitNewAnswer();
+                answerTask.Wait();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsNotNull(e);
+                Assert.AreEqual(expectedException.Message, e.InnerException.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SubmitNewAnswer_GetVehicles_Throws()
+        {
+            // Arrange
+            IOException expectedException = new IOException("failed...");
+            Mock<ICoxIntvApiService> apiService = new Mock<ICoxIntvApiService>();
+            apiService.Setup(x => x.GetVehicles(It.IsAny<string>()))
+                .Returns(Task.FromException<Vehicles>(expectedException));
+            ISubmitAnswerService answerService = new SubmitAnswerService(apiService.Object);
+
+
+            // Act
+            try
+            {
+                Task<AnswerServiceResponse> answerTask = answerService.SubmitNewAnswer();
+                answerTask.Wait();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsNotNull(e);
+                Assert.AreEqual(expectedException.Message, e.InnerException.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SubmitNewAnswer_GetVehicle_Throws()
+        {
+            // Arrange
+            IOException expectedException = new IOException("failed...");
+            Mock<ICoxIntvApiService> apiService = new Mock<ICoxIntvApiService>();
+            apiService.Setup(x => x.GetVehicle(It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(Task.FromException<CoxIntv.Model.Vehicles.Vehicle>(expectedException));
+            ISubmitAnswerService answerService = new SubmitAnswerService(apiService.Object);
+
+
+            // Act
+            try
+            {
+                Task<AnswerServiceResponse> answerTask = answerService.SubmitNewAnswer();
+                answerTask.Wait();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsNotNull(e);
+                Assert.AreEqual(expectedException.Message, e.InnerException.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SubmitNewAnswer_GetDealer_Throws()
+        {
+            // Arrange
+            IOException expectedException = new IOException("failed...");
+            Mock<ICoxIntvApiService> apiService = new Mock<ICoxIntvApiService>();
+            apiService.Setup(x => x.GetDealer(It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(Task.FromException<CoxIntv.Model.Dealers.Dealer>(expectedException));
+            ISubmitAnswerService answerService = new SubmitAnswerService(apiService.Object);
+
+
+            // Act
+            try
+            {
+                Task<AnswerServiceResponse> answerTask = answerService.SubmitNewAnswer();
+                answerTask.Wait();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.IsNotNull(e);
+                Assert.AreEqual(expectedException.Message, e.InnerException.Message);
+            }
+        }
+    }
+}
